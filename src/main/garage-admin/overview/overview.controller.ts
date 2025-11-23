b3d178ab9ad27e8c4d72b6ada92bc18772b823ad
masud2005 <masud20012005@gmail.com>
2025-11-23 06:08:58 +0600

    updated

--- src/main/garage-admin/overview/overview.controller.ts
+++ src/main/garage-admin/overview/overview.controller.ts
@@ -6,37 +6,37 @@ import { OverviewService } from './overview.service';
 @ApiTags('Garage Admin Overview')
 @Controller('garage-admin-overview')
 export class OverviewController {
-  constructor(private readonly overviewService: OverviewService) {}
+    constructor(private readonly overviewService: OverviewService) { }
 
-  @ApiBearerAuth()
-  @ValidateGarageOwner()
-  @ApiOperation({ summary: 'Get user overview statistics' })
-  @Get('stats')
-  async getUserOverview(@GetUser('userId') userId: string) {
-    return this.overviewService.getUserOverview(userId);
-  }
+    @ApiBearerAuth()
+    @ValidateGarageOwner()
+    @ApiOperation({ summary: 'Get user overview statistics' })
+    @Get('stats')
+    async getUserOverview(@GetUser('userId') userId: string) {
+        return this.overviewService.getUserOverview(userId);
+    }
 
-  @ApiBearerAuth()
-  @ValidateGarageOwner()
-  @ApiOperation({ summary: 'Get performance summary' })
-  @Get('performance-summary')
-  async getPerformanceSummary(@GetUser('userId') userId: string) {
-    return this.overviewService.getPerformanceSummary(userId);
-  }
+    @ApiBearerAuth()
+    @ValidateGarageOwner()
+    @ApiOperation({ summary: 'Get performance summary' })
+    @Get('performance-summary')
+    async getPerformanceSummary(@GetUser('userId') userId: string) {
+        return this.overviewService.getPerformanceSummary(userId);
+    }
 
-  @ApiBearerAuth()
-  @ValidateGarageOwner()
-  @ApiOperation({ summary: 'Get recent Acitivity' })
-  @Get('recent-activity')
-  async getRecentActivity(@GetUser('userId') userId: string) {
-    return this.overviewService.getRecentActivity(userId);
-  }
+    @ApiBearerAuth()
+    @ValidateGarageOwner()
+    @ApiOperation({ summary: 'Get recent Acitivity' })
+    @Get('recent-activity')
+    async getRecentActivity(@GetUser('userId') userId: string) {
+        return this.overviewService.getRecentActivity(userId);
+    }
 
-  @ApiBearerAuth()
-  @ValidateGarageOwner()
-  @ApiOperation({ summary: 'Get recent listing' })
-  @Get('recent-listings')
-  async getRecentListings(@GetUser('userId') userId: string) {
-    return this.overviewService.getRecentListings(userId);
-  }
+    @ApiBearerAuth()
+    @ValidateGarageOwner()
+    @ApiOperation({ summary: 'Get recent listing' })
+    @Get('recent-listings')
+    async getRecentListings(@GetUser('userId') userId: string) {
+        return this.overviewService.getRecentListings(userId);
+    }
 }
