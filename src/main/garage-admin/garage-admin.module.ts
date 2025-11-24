fb7a9548228dadb55924e49720387bb7a392524e
masud2005 <masud20012005@gmail.com>
2025-11-25 00:25:38 +0600

    subscription module boylar template added

--- src/main/garage-admin/garage-admin.module.ts
+++ src/main/garage-admin/garage-admin.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { PromotionalAdModule } from './promotional-ad/promotional-ad.module';
 import { OverviewModule } from './overview/overview.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [ProductModule, PromotionalAdModule, OverviewModule],
+  imports: [ProductModule, PromotionalAdModule, OverviewModule, SubscriptionModule],
 })
 export class GarageAdminModule {}
