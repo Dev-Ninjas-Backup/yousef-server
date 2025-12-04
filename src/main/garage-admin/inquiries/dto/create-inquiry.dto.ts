24a5d419aeffd8b6804e732ff35fd08b73d99fda
Joy43 <ssjoy43@gmail.com>
2025-12-04 22:25:10 +0600

    inquries message

--- src/main/garage-admin/inquiries/dto/create-inquiry.dto.ts
+++ src/main/garage-admin/inquiries/dto/create-inquiry.dto.ts
@@ -1,1 +1,12 @@
-export class CreateInquiryDto {}
+import { ContactSubject } from "@prisma/client";
+
+
+
+export class CreateInquiryDto {
+    FirstName: string;
+    LastName: string;
+    email: string;
+    subject: ContactSubject;
+    message: string;
+    garageOwnerId: string;
+}
