e32210bdb0db02649944b99f47421d3b43664f57
Joy43 <ssjoy43@gmail.com>
2025-12-30 01:14:43 +0600

    fix the auto approval

--- src/main/admin/admin-message/admin-setting/service/admin-setting.service.ts
+++ src/main/admin/admin-message/admin-setting/service/admin-setting.service.ts
@@ -7,7 +7,7 @@ import { UpdatePaymentConfigureDto } from '../dto/update-payment-configure.dto';
 
 @Injectable()
 export class AdminSettingService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   // ---------------platform setting create----
   @HandleError('Failed to create or update platform setting')
@@ -130,25 +130,24 @@ export class AdminSettingService {
     };
   }
 
-  // -----------auto approve garages-------------
+  // // -----------auto approve autoupdateApprovalSettingParts-------------
 
   @HandleError('failed to auto approve garages')
-  async updateApprovalSettingGarage() {
-    const updated = await this.prisma.user.updateMany({
+  async autoupdateApprovalSettingParts() {
+    const updated = await this.prisma.product.updateMany({
       where: {
-        role: 'GARAGE_OWNER',
-        garageStatus: 'PENDING',
-        isGarageVerified: false,
+        status: 'PENDING',
+      
       },
       data: {
-        garageStatus: 'APPROVE',
-        isGarageVerified: true,
+        status: 'APPROVED',
+       
       },
     });
 
     return {
       success: true,
-      message: `${updated.count} all garages auto-approved.`,
+      message: `${updated.count} products auto-approved.`,
     };
   }
 
