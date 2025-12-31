f34a696a40d3202f4b28d14b55c872335da1d844
Joy43 <ssjoy43@gmail.com>
2026-01-01 02:09:30 +0600

    fix the cros

--- src/main.ts
+++ src/main.ts
@@ -34,8 +34,8 @@ async function bootstrap() {
 
       const allowedOrigins = [
         'https://yousef-softvence.saikat.com.bd',
+        'https://yousef-frontend-nine.vercel.app',
         'http://13.62.72.14:3000',
-        'http://ec2-13-62-72-14.eu-north-1.compute.amazonaws.com:3000',
         'http://localhost:3000',
         'http://13.50.107.250:3000',
         'http://localhost:5173',
