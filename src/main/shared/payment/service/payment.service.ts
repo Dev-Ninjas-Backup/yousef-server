ca5fd330e585c9947d96ecf57b8a6faa00bd43fe
masud2005 <masud20012005@gmail.com>
2026-01-07 06:13:50 +0600

    payemnt success url query params cancel

--- src/main/shared/payment/service/payment.service.ts
+++ src/main/shared/payment/service/payment.service.ts
@@ -664,7 +664,7 @@ export class PaymentService {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.FRONTEND_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}&type=monthly`,
+      success_url: `${process.env.FRONTEND_URL}/payment-success`,
       cancel_url: `${process.env.FRONTEND_URL}/payment-cancel?type=monthly`,
 
       metadata: {
@@ -711,7 +711,7 @@ export class PaymentService {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.FRONTEND_URL}/garage-admin/my-products/add-product?session_id={CHECKOUT_SESSION_ID}&type=product_monthly`,
+      success_url: `${process.env.FRONTEND_URL}/payment-success`,
       cancel_url: `${process.env.FRONTEND_URL}/payment-cancel?type=product_monthly`,
       metadata: {
         userId,
@@ -754,7 +754,7 @@ export class PaymentService {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.FRONTEND_URL}/garage-admin/my-products/add-product?session_id={CHECKOUT_SESSION_ID}&type=pay_per`,
+      success_url: `${process.env.FRONTEND_URL}/payment-success`,
       cancel_url: `${process.env.FRONTEND_URL}/payment-cancel?type=pay_per`,
       metadata: {
         userId,
@@ -851,7 +851,7 @@ export class PaymentService {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.FRONTEND_URL}/garage-admin/my-products/add-product?session_id={CHECKOUT_SESSION_ID}&type=promotion`,
+      success_url: `${process.env.FRONTEND_URL}/payment-success`,
       cancel_url: `${process.env.FRONTEND_URL}/payment-cancel?type=promotion`,
       metadata: {
         userId,
